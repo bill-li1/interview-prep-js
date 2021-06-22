6205594d900db240cd945f647f135cc6e19f33f4
Bill Li <billli@gmail.com>
2021-06-22 10:55:50 -0400

    leetcode grind

---
+++ LeetCode/two_sum.js
@@ -0,0 +1,13 @@
+const twoSum = (nums, target) => {
+    let newmap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        newmap.set(nums[i], i);
+    }
+    console.log(nums.length);
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (newmap.has(complement) && newmap.get(complement) != i) {
+            return [i, newmap.get(complement)];
+        }
+    }
+};
